e7fff994e57364ce2d550305053096c128ae8fc2
Zhuo Lu <sethlu@mintkit.net>
2017-02-14 02:13:31 -0800

    Update static hiding test

--- src/inheritance/StaticPlayground.java
+++ src/inheritance/StaticPlayground.java
@@ -4,32 +4,35 @@
 
 package inheritance;
 
+class Machine {
+
+    /**
+     * Static method that the Machine
+     */
+    static void identify() {
+        System.out.println("Machine");
+    }
+}
+
+class Robot extends Machine {
+
+    static void identify() {
+        System.out.println("Robot");
+    }
+}
+
+class SteamMachine extends Machine {
+
+}
+
 public class StaticPlayground {
-
-    /**
-     *
-     */
-    static class Machine {
-
-        static void identify() {
-            System.out.println("Machine");
-        }
-    }
-
-    /**
-     *
-     */
-    static class Robot extends Machine {
-
-        static void identify() {
-            System.out.println("Robot");
-        }
-    }
 
     public static void main(String[] main) {
         testSuite1();
         testSuite2();
         testSuite3();
+        testSuite4();
+        testSuite5();
     }
 
     static void testSuite1() {
@@ -45,5 +48,15 @@ public class StaticPlayground {
     static void testSuite3() {
         Robot r = new Robot();
         r.identify();
+    }
+
+    static void testSuite4() {
+        Machine m = new SteamMachine();
+        m.identify();
+    }
+
+    static void testSuite5() {
+        SteamMachine m = new SteamMachine();
+        m.identify();
     }
 }
